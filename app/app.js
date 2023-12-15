95eda8a2bc081fed2c632930281b2f178d3ddd02
Steven H <58576896+geryonghost@users.noreply.github.com>
2023-12-14 21:21:11 -0600

    redirect to www

--- app/app.js
+++ app/app.js
@@ -1,24 +1,34 @@
 appEnvironment = process.env.environment
 
-const express = require('express');
-const vhost = require('vhost');
+const express = require('express')
+const vhost = require('vhost')
 
-const app = express();
+const app = express()
 
 // Define your different apps for each domain
-const appCOM = require('./com/appCOM');
-const appSGS = require('./sgs/appSGS');
+const appCOM = require('./com/appCOM')
+const appSGS = require('./sgs/appSGS')
 
 // Use vhost middleware to route requests based on domain
 if (appEnvironment == 'dev') {
-    app.use(vhost('dev.comtily.com', appCOM));
-    app.use(vhost('dev.skygatesecurity.com', appSGS));
+    app.use(vhost('dev.comtily.com', appCOM))
+    app.use(vhost('dev.skygatesecurity.com', appSGS))
+    
 } else {
-    app.use(vhost('www.comtily.com', appCOM));
-    app.use(vhost('www.skygatesecurity.com', appSGS));
+    app.use(vhost('www.comtily.com', appCOM))
+    app.use(vhost('www.skygatesecurity.com', appSGS))
+
+    app.use(vhost('comtily.com', function(req, res){
+      res.set('location', 'https://www.comtily.com');
+      res.status(301).send()
+    }))
+    app.use(vhost('skygatesecurity.com', function(req, res){
+      res.set('location', 'https://www.skygatesecurity.com');
+      res.status(301).send()
+    }))
 }
 // Add a default route or handle unrecognized domains
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000')
 });
