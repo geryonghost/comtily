e2524752500ab6f55306a4ddcd6ec703cea45799
Steven H <58576896+geryonghost@users.noreply.github.com>
2023-12-14 20:42:56 -0600

    comtily and sgs

---
+++ README.md
@@ -0,0 +1,26 @@
+# Configure Dev Environment
+```
+npm install express
+npm install ejs
+npm install vhost
+npm install nodemon
+```
+
+# Local Hosts
+## Mac
+`sudo nano /private/etc/hosts`
+
+## Windows
+`runas /user:<localmachinename>\administrator notepad c:\windows\systems\drivers\etc\hosts`
+```
+127.0.0.1   dev.comtily.com
+127.0.0.1   dev.itsweatheroutside.com
+127.0.0.1   dev.notscrapyet.com
+127.0.0.1   dev.skygatesecurity.com
+```
+
+# Viewing from Browser
+http://dev.comtily.com:3000
+http://dev.itsweatheroutside.com:3000
+http://dev.notscrapyet.com:3000
+http://dev.skygatesecurity.com:3000
---
+++ app/com/appCOM.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const { render } = require("ejs")
+
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/views`)
+app.use(express.static(`${__dirname}/public`))
+
+// Default home page / index
+app.get('/', (req, res) => {
+  const pageTitle = "Comtily"
+  res.render('index', { pageTitle: pageTitle })
+});
+
+// Auto Insurance
+app.get('/autoinsurance', (req, res) => {
+  const pageTitle = 'Auto Insurance'
+  res.render('autoinsurance', { pageTitle: pageTitle })
+});
+
+// How to Contribute
+app.get('/howtocontribute', (req, res) => {
+  const pageTitle = 'How to Contribute'
+  res.render('howtocontribute', { pageTitle: pageTitle })
+});
+
+// It's Weather Outside
+app.get('/itsweatheroutside', (req, res) => {
+  const pageTitle = 'It\s Weather Outside'
+  res.render('itsweatheroutside', { pageTitle: pageTitle })
+});
+
+// Not Scrap Yet
+app.get('/notscrapyet', (req, res) => {
+  const pageTitle = 'Not Scrap Yet'
+  res.render('notscrapyet', { pageTitle: pageTitle })
+});
+
+// Skygate Security
+app.get('/skygatesecurity', (req, res) => {
+  const pageTitle = 'Skygate Security'
+  res.render('skygatesecurity', { pageTitle: pageTitle})
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.redirect('/')
+});
+
+module.exports = app
