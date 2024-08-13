203ce60903bd342fa2ed3cc725324aece5a5c1e4
geryonghost <58576896+geryonghost@users.noreply.github.com>
2024-08-12 21:38:09 -0500

    transition to app.behavio.cc

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ const vhost = require('vhost')
 const app = express()
 
 // Define your different apps for each domain
-const appBEH = require('./beh/appBEH')
+const appBEH = require('./beh_app/appBEH')
 const appIWO = require('./iwo/appIWO')
 const appSWH = require('./swh/appSWH')
 
@@ -17,16 +17,10 @@ if (appEnvironment == 'dev') {
     app.use(vhost('dev.itsweatheroutside.com', appIWO))
     app.use(vhost('dev.thehillden.us', appSWH))
 } else {
-    app.use(vhost('behavio.cc', appBEH))
+    app.use(vhost('app.behavio.cc', appBEH))
     app.use(vhost('itsweatheroutside.com', appIWO))
     app.use(vhost('steven.thehillden.us', appSWH))
 
-    app.use(
-        vhost('www.behaivo.cc', function (req, res) {
-            res.set('location', 'https://behaivo.cc')
-            res.status(301).send()
-        })
-    )
     app.use(
         vhost('www.itsweatheroutside.com', function (req, res) {
             res.set('location', 'https://itsweatheroutside.com')
